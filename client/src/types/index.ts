67772662f324e05229078b5bf0c980811b19d34c
Manik-Arora <manikarora2605@gmail.com>
2026-06-06 00:38:51 +0530

    Initializing client

---
+++ client/src/types/index.ts
@@ -0,0 +1,37 @@
+export interface User {
+  id: string;
+  email: string;
+  fullName?: string;
+  imageUrl?: string;
+  name?: string;
+  image?: string;
+}
+
+export interface Message {
+  id: string;
+  role: any;
+  content: string;
+  timestamp: string;
+}
+
+export interface Version {
+  id: string;
+  timestamp: string;
+  code: string;
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  initialPrompt: string;
+  currentCode: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  user?: string;
+  isPublished?: boolean;
+  versionId?: string;
+  conversation: Message[];
+  versions: Version[];
+  currentVersionIndex: string;
+}
